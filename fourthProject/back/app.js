feb3faa3977a0f0bc30d209db6b2f8c7e9a7934a
Gwak-daeun <bbibibbibi22@gmail.com>
2023-03-16 17:17:28 +0900

    Update app.js

--- fourthProject/back/app.js
+++ fourthProject/back/app.js
@@ -55,7 +55,7 @@ app.use(session({
   cookie: {
     httpOnly: true,
     secure: false,
-    domain: process.env.NODE_ENV === 'production' && 'http://fashionary.site'
+    domain: process.env.NODE_ENV === 'production' && '.fashionary.site'
   }
 }));
 app.use(passport.initialize());
